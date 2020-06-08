944e0fede4df3a9ed3f90fc48e9ef4e3ce69386d
Clemens Retra <beemyo.official@gmail.com>
2020-06-08 12:19:55 +0200

    added multiple layers

--- main.py
+++ main.py
@@ -10,5 +10,12 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2, 3, 0.5]
 
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
+weights2 = [[0.2, 0.8, -0.5],
+           [0.5, -0.91, 0.26],
+           [-0.26, -0.27, 0.17]]
+
+biases2 = [3, 1, 0.5]
+
+output_1 = np.dot(inputs, np.array(weights).T) + biases
+output_2 = np.dot(output_1, np.array(weights2).T) + biases2
+print(output_1)
