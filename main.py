9265c177a7d87c31522afb90dd631ce4b24c75e1
Clemens Retra <beemyo.official@gmail.com>
2020-06-08 12:37:18 +0200

    added layer objects with weights

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
-          [2, 5, -1, 2],
-          [-1.5, 2.7, 3.3, -0.8]]
+np.random.seed(0)
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+X = [[1, 2, 3, 2.5],
+     [2, 5, -1, 2],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-biases = [2, 3, 0.5]
-
-weights2 = [[0.2, 0.8, -0.5],
-           [0.5, -0.91, 0.26],
-           [-0.26, -0.27, 0.17]]
-
-biases2 = [3, 1, 0.5]
-
-output_1 = np.dot(inputs, np.array(weights).T) + biases
-output_2 = np.dot(output_1, np.array(weights2).T) + biases2
-print(output_1)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = np.random.randn(n_inputs, n_neurons)
+    def forward(self):
+        pass
